84c9f276c460e3532eab9c6f4c6baa092dc51f64
moose <maximumquirk10@gmail.com>
2022-10-13 08:35:54 -0500

    basic animation of snake being able to move from one to another

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
 from turtle import Screen as s
 from turtle import Turtle as t
+import time
 screen = s()
 screen.setup(width = 600, height = 600)
 screen.bgcolor('black')
 screen.title('Snake')
 
-one = t()
-two = t()
-three = t()
-blocks = [one,two,three]
-start_pos=[(0,0),(-20,0),(-40,0)]
-pos = 0
-for i in blocks:
-    i.penup()
-    i.shape('square')
-    i.color('white')
-    i.back(pos)
-    pos = pos + 20
 
+start_pos = [(0, 0), (-20, 0), (-40, 0)]
+segments = []
 
+for position in start_pos:
+    new_segment = t("square")
+    new_segment.penup()
+    new_segment.color("white")
+    new_segment.goto(position)
+    segments.append(new_segment)
 
+game_is_on = True
 
+while game_is_on:
+    screen.update()
+    time.sleep(0.2)
+
+    for seg_num in range(len(segments)-1, 0, -1):
+        new_x = segments[seg_num -1].xcor()
+        new_y = segments[seg_num - 1].ycor()
+        segments[seg_num].goto(new_x,new_y)
+    segments[0].forward(20)
 screen.exitonclick()
