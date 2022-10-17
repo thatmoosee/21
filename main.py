b0391c90a8d5ebedb56706cdcbd8a6d725c085a3
moose <maximumquirk10@gmail.com>
2022-10-17 08:42:47 -0500

    food and can identify if touch food

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from turtle import Screen as s
 from turtle import Turtle as t
 from snake import Snake
-
+from food import Food
+import time
 screen = s()
 snake = Snake()
 snake.create_snake()
-snake.add_snake()
 screen.listen()
+food = Food()
 
 game_is_on = True
-while game_is_on:
-    screen.onkey(snake.up, "Up")
-    screen.onkey(snake.left, "Left")
-    screen.onkey(snake.right, "Right")
-    screen.onkey(snake.down, "Down")
-    snake.move()
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
+screen.onkey(snake.down, "Down")
 
-screen.exitonclick()
+while game_is_on:
+    screen.update()
+    time.sleep(0.1)
+    snake.move()
+    if snake.head.distance(food) < 15:
+        food.refresh()
--- snake.py
+++ snake.py
@@ -29,15 +29,15 @@ class Snake:
 
     def move(self):
         game_is_on = True
-        while game_is_on:
-            screen.update()
-            time.sleep(0.2)
 
-            for seg_num in range(len(self.segments) - 1, 0, -1):
-                new_x = self.segments[seg_num - 1].xcor()
-                new_y = self.segments[seg_num - 1].ycor()
-                self.segments[seg_num].goto(new_x, new_y)
-            self.head.forward(20)
+        screen.update()
+        time.sleep(0.2)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
+        self.head.forward(20)
+
     def add_snake(self):
         new_segment = t("square")
         new_segment.penup()
